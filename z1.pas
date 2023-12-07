de5d80db8f16875d5db5d2a1aa3d328095962800
akmavv <144677328+akmavv@users.noreply.github.com>
2023-12-07 11:11:21 +0300

    Add files via upload

---
+++ z1.pas
@@ -0,0 +1,38 @@
+﻿program z1;
+var
+  arr: array[1..100] of integer;
+  n, i, j, c: integer;
+  d: boolean;
+
+begin
+  write('Введите количество элементов в массиве: ');
+  readln(n);
+  
+  write('Введите элементы массива: ');
+  for i := 1 to n do
+  begin
+    read(arr[i]);
+  end;
+  
+  c:= 0;
+  
+  for i := 1 to n do
+  begin
+    d := true;
+    for j := 1 to i - 1 do
+    begin
+      if arr[i] = arr[j] then
+      begin
+        d := false;
+        break;
+      end;
+    end;
+    
+    if d then
+    begin
+      c := c + 1;
+    end;
+  end;
+  
+  writeln('Количество различных элементов в массиве равно: ', c);
+end.
---
+++ z2.pas
@@ -0,0 +1,27 @@
+﻿program z1;
+
+var
+  a: string;
+  w, l, i: integer;
+
+begin
+  w := 0;
+  l := 0;
+  
+  writeln('Введите строку: ');
+  readln(a);
+  
+  for i := 1 to length(a) do
+  begin
+    if (a[i] = ' ') or (a[i] = ',') or (a[i] = '.') or (a[i] = '!') or (a[i] = '?') then
+      w := w + 1
+    else
+      l := l + 1;
+  end;
+  
+  if l > 0 then
+    w := w + 1;
+  
+  writeln('Число слов: ', w);
+  writeln('Число букв: ', l);
+end.
